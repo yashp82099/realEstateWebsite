b9dc462785e04a65339b65a0a0372db86c9e8009
yashp82099 <yash82099@yahoo.com>
2021-09-27 20:23:08 -0400

    got the api to work

--- js/index.js
+++ js/index.js
@@ -1,6 +1,28 @@
+// const { Carousel } = require("bootstrap");
+
 window.addEventListener('DOMContentLoaded', () => {
-    fetch('https://api.bridgedataoutput.com/api/v2/test/listings?access_token=6baca547742c6f96a6ff71b138424f21&limit=50')
+    let pot = document.getElementsByClassName('port-area')
+    fetch('https://api.bridgedataoutput.com/api/v2/test/listings?access_token=6baca547742c6f96a6ff71b138424f21&limit=5')
     .then(response => response.json())
-    .then(data => console.log(data['bundle'][0]))
+    .then(data => listingHouses(data['bundle'],pot) )
 
 });
+
+
+function listingHouses(listing,pot){
+    console.log(listing.length)
+    let listnum = 0
+    
+    for(let i = 0; i < listing.length;i++){
+        
+        //console.log(piclist);
+        
+        console.log(listing[i]['Media'][0]['MediaURL'])
+        let img = document.createElement('img')
+        img.src = listing[i]['Media'][0]['MediaURL']
+        // piclist.appendChild(img)
+        document.querySelector(".port-area").appendChild(img)
+        console.log(document.querySelector(".listing"));
+    }
+    
+}

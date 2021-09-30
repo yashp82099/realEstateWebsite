0996ea1ca59a93a90a909bcfcf8c3b8f02715dd7
yashp82099 <yash82099@yahoo.com>
2021-09-30 18:57:59 -0400

    style home page

--- js/index.js
+++ js/index.js
@@ -1,28 +1,38 @@
 // const { Carousel } = require("bootstrap");
 
+
 window.addEventListener('DOMContentLoaded', () => {
-    let pot = document.getElementsByClassName('port-area')
+    
+    delayedGreeting();
     fetch('https://api.bridgedataoutput.com/api/v2/test/listings?access_token=6baca547742c6f96a6ff71b138424f21&limit=5')
     .then(response => response.json())
-    .then(data => listingHouses(data['bundle'],pot) )
+    .then(data => listingHouses(data['bundle']) )
 
 });
 
 
 function listingHouses(listing,pot){
-    console.log(listing.length)
-    let listnum = 0
-    
-    for(let i = 0; i < listing.length;i++){
-        
-        //console.log(piclist);
-        
-        console.log(listing[i]['Media'][0]['MediaURL'])
-        let img = document.createElement('img')
-        img.src = listing[i]['Media'][0]['MediaURL']
-        // piclist.appendChild(img)
-        document.querySelector(".port-area").appendChild(img)
-        console.log(document.querySelector(".listing"));
-    }
-    
+    let div = document.querySelector('slideshow')
+ 
 }
+
+
+function sleep(ms) {
+    return new Promise(
+      resolve => setTimeout(resolve, ms)
+    );
+  }
+  
+  async function delayedGreeting() {
+    console.log("Hello");
+    await sleep(2000);
+    console.log("World!");
+    await sleep(2000);
+    console.log("Goodbye!");
+    for (let index = 0; index < 5; index++) {
+        await sleep(2000);
+        console.log(index);
+        
+    }    
+}
+  

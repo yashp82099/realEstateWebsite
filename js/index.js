ad6fda3563fb9a8be4cf00924678bba24f99ffec
yashp82099 <yash82099@yahoo.com>
2021-09-30 19:20:19 -0400

    make auto sideshow

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
-// const { Carousel } = require("bootstrap");
-
 
 window.addEventListener('DOMContentLoaded', () => {
     
-    delayedGreeting();
+    // delayedGreeting();
     fetch('https://api.bridgedataoutput.com/api/v2/test/listings?access_token=6baca547742c6f96a6ff71b138424f21&limit=5')
     .then(response => response.json())
     .then(data => listingHouses(data['bundle']) )
@@ -11,28 +9,44 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 
-function listingHouses(listing,pot){
-    let div = document.querySelector('slideshow')
- 
+async function listingHouses(listing){
+    let div = document.querySelector('.slideshow')
+
+    for (let i = 0; i < listing.length; i++) {
+        let img = document.createElement('img')
+        console.log(listing[i]["Media"][1]['MediaURL']);
+        img.src = listing[i]['Media'][1]['MediaURL']
+        // img.width = '100%'
+        img.classList.add('slideimg')
+        div.appendChild(img)
+        await delay(3)
+        div.innerHTML = ""
+    }
+     
 }
 
 
-function sleep(ms) {
-    return new Promise(
-      resolve => setTimeout(resolve, ms)
-    );
-  }
+// function sleep(ms) {
+//     return new Promise(
+//       resolve => setTimeout(resolve, ms)
+//     );
+//   }
   
-  async function delayedGreeting() {
-    console.log("Hello");
-    await sleep(2000);
-    console.log("World!");
-    await sleep(2000);
-    console.log("Goodbye!");
-    for (let index = 0; index < 5; index++) {
-        await sleep(2000);
-        console.log(index);
+//   async function delayedGreeting() {
+//     console.log("Hello");
+//     await sleep(2000);
+//     console.log("World!");
+//     await sleep(2000);
+//     console.log("Goodbye!");
+//     for (let index = 0; index < 5; index++) {
+//         await sleep(2000);
+//         console.log(index);
         
-    }    
+//     }    
+// }
+
+function delay(n){
+    return new Promise(function(resolve){
+        setTimeout(resolve,n*1000);
+    });
 }
-  
